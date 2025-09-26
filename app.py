8c1a31df46d0457c1a016cd16eb0a03c7392ee06
eagle030 <srsnvvv@gmail.com>
2025-09-26 21:40:05 +0300

    added endpoint create

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# "База" в памяти
+forms = []
+counter = 1
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route("/")
+def index():
+    return render_template("index.html", forms=forms)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    global counter
+    if request.method == "POST":
+        new_form = {
+            "id": counter,
+            "name": request.form.get("name"),
+            "email": request.form.get("email"),
+            "category": request.form.get("category")
+        }
+        forms.append(new_form)
+        counter += 1
+        return redirect(url_for("index"))
+    return render_template("create.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
